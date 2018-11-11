e141b1f41ce69986e06e77401f835f462a00b13c
marvinpjr <marvin@marvinpalmer.com>
2018-11-11 15:40:29 -0600

    pulling out vue material framework, still not sure what to use

--- web/src/main.js
+++ web/src/main.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/black-green-dark.css' 
 import App from './App.vue'
 
-// not recommended to pull in the entire VueMaterial bundle, will affect performance
-// when we figure out what we are actually using, we can bring in only the stuff we are actually using
-// ie. import { MdButton, MdContent } from 'vue-material/dist/components' and
-// Vue.use(MdButton)
-// Vue.use(MdContent)
-Vue.use(VueMaterial)
 Vue.config.productionTip = false
 
 new Vue({
